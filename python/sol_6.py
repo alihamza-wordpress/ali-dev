813542fb9fbcf55228ca95c682265b60c896f6b5
Muhammad Ali Hamza <wordpresswithali44@gmail.com>
2026-02-12 10:01:12 +0500

    problem solutions

---
+++ python/sol_6.py
@@ -0,0 +1,8 @@
+number = 10
+factorial = 1
+
+while number >= 1:
+    factorial *= number
+    number -= 1
+
+print("factorial: ", factorial)
---
+++ python/sol_7.py
@@ -0,0 +1,7 @@
+while True:
+    number =int(input("Enter value between 1 and 10: "))
+    if 1 <= number <= 10:
+        print("thank you")
+        break
+    else :
+        print("No value, Please try again")
