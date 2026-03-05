a161cf52a933d1b53db3ecfea1f117008d7be0eb
Muhammad Ali Hamza <wordpresswithali44@gmail.com>
2026-03-05 09:29:42 +0500

    practice

---
+++ basic_python/practice.py
@@ -0,0 +1,11 @@
+name = "harry"
+
+print(name[-4:-1])#both has same output 
+print(name[1:4])
+
+
+print(name.startswith("ha"))# checks name start with ha
+print(name.endswith("rry"))# checks name end with rry
+print(name.capitalize())# capitalize first letter 
+print(name.replace("harry", "ali")) #replace harry with ali 
+
