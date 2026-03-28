cb8bfd57ae5010dc16e327115f5958a3260fe52a
Muhammad Ali Hamza <wordpresswithali44@gmail.com>
2026-03-28 12:05:54 +0500

    project 2

--- Projects/Student Management System/main.py
+++ Projects/Student Management System/main.py
@@ -39,20 +39,67 @@ def search_student():
         for s in students:
             if s["id"] == id:
                 print("Student found")
-                print(S)
+                print(s)
                 return
             else:
                 print("Student not found")
     elif choice == "2":
         id = int(input("Enter name: "))
         for s in students:
-            if s["name"] == name :
+            if s["name"] == name:
                 print("Student found")
-                print(S)
+                print(s)
                 return
             else:
                 print("Student not found")
     else:
         print("invalid choice\n")
 
-#Under Development
+def update_student():
+    id = int(input("Enter id to update: "))
+
+    for s in students:
+        if s["id"] == id:
+            s["name"] = input("Enter new name: ")
+            s["age"] = int(input("Enter new age: "))
+            s["course"] = input("Enter new course: ")
+
+            print("Student updated\n")
+            return
+        
+    print("No student found\n")
+
+def delete_student():
+    id = int(input(" Enter id to delete: "))
+
+    for s in students:
+        if s["id"] == id:
+            students.remove(s)
+            print("Student deleted")
+
+while True:
+    print("__________________")
+    print("1. add student")    
+    print("2. view student")    
+    print("3. Search student")    
+    print("4. Update student")    
+    print("5. Delete student")    
+    print("6. exit")
+
+    choice= input("Enter choice: ")
+
+    if choice == "1":
+        add_student()
+    elif choice == "2":
+        view_student()
+    elif choice == "3":
+        search_student()
+    elif choice == "4":
+        update_student()
+    elif choice == "5":
+        delete_student()
+    elif choice == "6":
+        print("Goodbye! You have successfully exit from the program. Enjoy your life")
+        break
+    else:
+        print("invalid choice\n")
